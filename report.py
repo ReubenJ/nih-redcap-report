971b71ef3a6a91b316c5538b183ce769cdf0a902
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2020-08-13 21:47:39 -0500

    UI working

--- report.py
+++ report.py
@@ -71,6 +71,9 @@ class ReportFrame(wx.Frame):
         self.output_sizer = wx.GridBagSizer(11, 11)
 
         title = wx.StaticText(self.panel, label='Cumulative Enrollment Report')
+        self.error = wx.StaticText(self.panel)
+        self.error.Hide()
+        self.error.SetForegroundColour(wx.RED)
         self.bold = wx.Font(18, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         self.small_bold = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         title.SetFont(self.bold)
@@ -82,6 +85,7 @@ class ReportFrame(wx.Frame):
         self.protocol_list = wx.ListBox(self.panel, style=wx.LB_MULTIPLE)
         # self.output = wx.StaticText(self.panel, label='0')
         update = wx.Button(self.panel, label='Update')
+        update.SetDefault()
         clear = wx.Button(self.panel, label='Reset')
 
         update.Bind(wx.EVT_BUTTON, self.update)
@@ -97,6 +101,7 @@ class ReportFrame(wx.Frame):
         self.input_sizer.Add(update, 0, wx.ALL | wx.CENTER, 5)
         self.input_sizer.Add(clear, 0, wx.ALL | wx.CENTER, 5)
         self.sizer.Add(self.input_sizer, 0, wx.ALL | wx.ALIGN_CENTER, 5)
+        self.sizer.Add(self.error, 0, wx.ALL | wx.ALIGN_CENTER, 5)
         self.sizer.Add(wx.StaticLine(self.panel, -1),
                        flag=wx.ALL | wx.EXPAND, border=5)
         # self.sizer.Add(self.panel, 0, wx.ALL | wx.ALIGN_CENTER, 5)
@@ -110,15 +115,21 @@ class ReportFrame(wx.Frame):
         frame = APIKeyFrame(parent=self)
 
     def fill_options(self):
+        self.grant_list.InsertItems([g.replace(' ', '').split(',')[1] for g in self.project.export_metadata(
+            fields=['grant'])[0]['select_choices_or_calculations'].split('|')], 0)
+        self.protocol_list.InsertItems([p.replace(' ', '').split(',')[1] for p in self.project.export_metadata(
+            fields=['protocol'])[0]['select_choices_or_calculations'].split('|')], 0)
+        self.fill_output()
+        self.sizer.Add(self.output_sizer, 0, wx.ALL | wx.ALIGN_CENTER, 5)
+        self.sizer.AddSpacer(20)
+        self.sizer.Fit(self)
+
+    def fill_output(self):
         cat = wx.StaticText(self.panel, label='Ethnic Categories')
         self.output_sizer.Add(cat, pos=(0, 1),
                               span=wx.GBSpan(1, 16), flag=wx.ALIGN_CENTER)
         self.output_sizer.Add(wx.StaticLine(self.panel, -1),
                               pos=(1, 1), flag=wx.ALL | wx.EXPAND, border=5, span=wx.GBSpan(1, 16))
-        self.grant_list.InsertItems([g.replace(' ', '').split(',')[1] for g in self.project.export_metadata(
-            fields=['grant'])[0]['select_choices_or_calculations'].split('|')], 0)
-        self.protocol_list.InsertItems([p.replace(' ', '').split(',')[1] for p in self.project.export_metadata(
-            fields=['protocol'])[0]['select_choices_or_calculations'].split('|')], 0)
 
         global RACES
         RACES = [r.split(', ')[1].strip() for r in self.project.export_metadata(
@@ -138,9 +149,6 @@ class ReportFrame(wx.Frame):
         # self.print_grid_bag(self.output_sizer)
         self.output_sizer.Add(wx.StaticText(self.panel, label='<- Total Enrolled'), pos=(12, 15),
                               flag=wx.ALL | wx.ALIGN_RIGHT, border=5)
-        self.sizer.Add(self.output_sizer, 0, wx.ALL | wx.ALIGN_CENTER, 5)
-        self.sizer.AddSpacer(20)
-        self.sizer.Fit(self)
 
     def fill_races(self):
         i_race = 0
@@ -197,17 +205,25 @@ class ReportFrame(wx.Frame):
                                   flag=wx.ALIGN_CENTER, border=5)
 
     def update(self, evt):
+        self.error.Hide()
         if self.start_date.GetValue() > self.end_date.GetValue():
+            self.error.SetLabelText('Start date must be earlier than end date.')
+            self.error.SetForegroundColour(wx.RED)
+            self.error.Show()
+            self.sizer.Fit(self)
+            return
 
-        self.enrollments = self.project.export_records(raw_or_label='label', fields=['enrollment',
-                                                                                     'grant',
-                                                                                     'protocol'])
-        print(self.get_number_of_grants_protocols())
+        self.enrollments = self.project.export_records(raw_or_label='label', fields=['enrollment', 'grant', 'protocol'])
         grant_selection = [self.grant_list.GetItems()[i] for i in self.grant_list.GetSelections()]
         protocol_selection = [self.protocol_list.GetItems()[i] for i in self.protocol_list.GetSelections()]
         ids = self.filter_enrollments(protocol_selection, grant_selection)
-        participants = self.project.export_records(records=ids, raw_or_label='label', fields=['gender', 'ethnicity', 'race'])
-        self.fill_table(participants)
+        if ids:
+            participants = self.project.export_records(records=ids, raw_or_label='label', fields=['gender', 'ethnicity', 'race'])
+            self.fill_table(participants=participants)
+        else:
+            self.fill_table(string='0')
+
+        self.sizer.Fit(self)
 
     def filter_enrollments(self, pr_sel, gr_sel):
         ids = []
@@ -220,6 +236,56 @@ class ReportFrame(wx.Frame):
                         ids.append(e.get('record_id'))
         print(ids)
         return ids
+
+    def fill_table(self, participants=None, string=None):
+        race_totals = {}
+        for eth in range(len(ETHNICITIES)):
+            for gen in range(len(GENDERS)):
+                offset = 0
+                gen_tot = 0
+                for race in range(len(RACES)):
+                    if RACES[race] == 'Black':
+                        offset = -1
+                        continue
+                    count = 0
+                    if not string:
+                        count = self.get_count_with_filter(participants, RACES[race], GENDERS[gen], ETHNICITIES[eth])
+                    gen_tot += count
+                    race_totals[RACES[race]] = race_totals.get(RACES[race], 0) + count
+                    race_total_text = self.output_sizer.FindItemAtPosition((race + 4 + offset, 14))
+                    if race_total_text:
+                        if not string:
+                            race_total_text.GetWindow().SetLabelText(str(race_totals[RACES[race]]))
+                        else:
+                            race_total_text.GetWindow().SetLabelText(string)
+
+                    text = self.output_sizer.FindItemAtPosition((race + 4 + offset, (eth * 4) + gen + 2))
+                    if text:
+                        if not string:
+                            text.GetWindow().SetLabelText(str(count))
+                        else:
+                            text.GetWindow().SetLabelText(string)
+                text = self.output_sizer.FindItemAtPosition((len(RACES) + 1 + 4 + offset, (eth * 4) + gen + 2))
+                if text:
+                    if not string:
+                        text.GetWindow().SetLabelText(str(gen_tot))
+                    else:
+                        text.GetWindow().SetLabelText(string)
+        text = self.output_sizer.FindItemAtPosition((12, 14))
+        if text:
+            if not string:
+                text.GetWindow().SetLabelText(str(len(participants)))
+            else:
+                text.GetWindow().SetLabelText(string)
+
+
+    def get_count_with_filter(self, participants, race, gender, ethnicity):
+        count = 0
+        for p in participants:
+            if p['race'] == race and p['gender'] == gender and p['ethnicity'] == ethnicity:
+                count += 1
+
+        return count
 
     def clear(self, evt):
         self.grant_list.SetSelection(-1)
